ab4e9ef0751844f815f6642a763fbc08f8b41105
SOne <sonetrue1@gmail.com>
2025-08-03 15:44:03 +0300

    Test CI/CD pipeline 2

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return jsonify({"message": "Привет от backend-а и меня!"
-                               "\n И всё удачно работает!"})
+                               "\n И всё удачно работает!"
+                               "\n А так же не прерывная доставка!"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
