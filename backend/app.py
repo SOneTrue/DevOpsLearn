6cd462ac8c8091cff24105e9c6dccc597c507be7
SOne <sonetrue1@gmail.com>
2025-08-03 15:50:05 +0300

    Test .env 2

--- backend/app.py
+++ backend/app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return f"{os.getenv('GREETING', 'Значение не найдено')}"
-
+    return f"{os.getenv('GREETING', 'Значение не найдено, или ты криворукий!')}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
